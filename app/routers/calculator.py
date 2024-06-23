706c6389cdd9faffe8cf871b03a79694914a33cd
natalia-coelho <nataliacoelho199@gmail.com>
2024-06-23 19:36:40 -0300

    Implementing calculator tests

--- app/routers/calculator.py
+++ app/routers/calculator.py
@@ -4,16 +4,16 @@ router = APIRouter()
 
 @router.post("/calculator/sum")
 def sum(number1: int, number2: int):
-    return {"Result": number1 + number2}
+    return number1 + number2
 
 @router.post("/calculator/subtract")
 def subtract(number1: int, number2: int):
-    return {"Result": number1 - number2}
+    return number1 - number2
 
 @router.post("/calculator/multiply")
 def multiply(number1: int, number2: int):
-    return {"Result": number1 * number2}
+    return number1 * number2
 
 @router.post("/calculator/divide")
 def divide(number1: int, number2: int):
-    return {"Result": number1 / number2}
+    return number1 / number2
